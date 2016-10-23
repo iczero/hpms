25b11773cd9d9289047dbb730f52e41fcebec873
Jeff Luo <jeffluo35@gmail.com>
2016-10-22 19:36:50 -0700

    Make test more verbose

--- test/test.js
+++ test/test.js
@@ -3,8 +3,11 @@
 var is_fine = true;
 
 // is everything fine?
+console.log('[INFO] Test started');
 if (is_fine) {
+    console.log('[INFO] Test succeeded');
     process.exit(0); // everything is fine
 } else {
+    console.log('[CRITICAL] Test failed!');
     process.exit(1); // something is not fine
 }
