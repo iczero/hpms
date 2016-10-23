8c56d94faef3d5930c60622d2d36bdf1dd3601eb
jeffl35 <jeffluo35@gmail.com>
2016-10-22 18:32:38 -0700

    Fix tests

---
+++ test/test.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+var is_fine = true;
+
+// is everything fine?
+if (is_fine) {
+    process.exit(0); // everything is fine
+} else {
+    process.exit(1); // something is not fine
+}
